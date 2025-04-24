7966e06779daf113f6b5fd2bc3a3560349d2fc52
= <wlxwulixing@gmail.com>
2025-04-25 00:52:38 +0800

    till oop

---
+++ hangman_words_45.py
@@ -0,0 +1,10 @@
+# Define word categories
+animals = ["dog", "cat", "elephant", "tiger", "lion", "giraffe", "whale", "panda", "koala", "kangaroo"]
+colors = ["red", "blue", "green", "yellow", "pink", "purple", "orange", "black", "white", "brown"]
+objects = ["table", "chair", "computer", "phone", "book", "pen", "keyboard", "mouse", "lamp", "cup"]
+adjectives = ["happy", "sad", "bright", "dark", "beautiful", "angry", "silent", "loud", "fast", "slow"]
+places = ["city", "forest", "beach", "mountain", "desert", "village", "town", "ocean", "lake", "river"]
+verbs = ["run", "jump", "swim", "fly", "read", "write", "sing", "dance", "eat", "play"]
+
+# Combine all the word categories
+words = animals + colors + objects + adjectives + places + verbs
